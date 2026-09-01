f87f021f05d390e0bbd918c200eab04422e0dc89
voldemore0509 <vdchanudet@gmail.com>
2026-09-01 20:04:16 +0200

    Allocation mémoire et code propre

--- core/QuickDeletion.c
+++ core/QuickDeletion.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
 
-struct state info;
+struct stat info;
 
 int cst(char *path){
     int i = 0;
-    while(path[i] != "\0"){
+    while(path[i] != '\0'){
         i ++;
     }
-    return i++;
+    return i;
 }
 
-int modularity(char *path){
+int modularity(char *path,char **out){
     char *writingpath = malloc(sizeof(char)*100);
     int result_cst = cst(path);
-    char *rbt = realloc(writingpath,result_cst);
+    char *rbt = realloc(writingpath,result_cst+1);
     if(rbt != NULL){
-        writingpath = rbt;  
+        writingpath = rbt;
+        strcpy(writingpath, path);  
+        *out = writingpath;
         return 0;
     }
+    free(writingpath);
     return 2;
 }
 
@@ -27,11 +33,16 @@ int clear_memory(char *path){
     return 3;
 }
 
-int deletion_process(char *path , int size){
-    int result = state(path,&info);
+int deletion_process(char *path , int size, char **out){
+    int result_allocation_memory = modularity(path ,out);
+    if(result_allocation_memory != 0){
+        return 2;
+    }
+    int result = stat(path,&info);
     if(result != 0){
         return 1;
     }
+    clear_memory(*out);
     return 0;
 }
 
